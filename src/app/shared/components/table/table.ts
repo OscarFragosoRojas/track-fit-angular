3b1c0690b29a6274254749fe12fd9946df58dfca
OscarFragosoRojas <oscarfragosorojas@gmail.com>
2026-06-19 14:03:45 -0600

    feat: implement custom glassmorphism styling for data tables in both light and dark modes

--- src/app/shared/components/table/table.ts
+++ src/app/shared/components/table/table.ts
@@ -8,19 +8,21 @@ import { Table, TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableColumn } from './types/table-types';
+import { Card } from '../cards/card/card';
 
 
 @Component({
     selector: 'app-table',
     templateUrl: './table.html',
     standalone: true,
-    imports: [SelectModule, IconFieldModule, InputIconModule, MultiSelectModule, TableModule, TagModule, InputTextModule, FormsModule],
+    imports: [SelectModule, IconFieldModule, InputIconModule, MultiSelectModule, TableModule, TagModule, InputTextModule, FormsModule, Card],
     providers: []
 })
 export class TableComponent<T> {
 
     data = input.required<T[]>();
     columns = input.required<TableColumn[]>();
+    searchPlaceholder = input<string>('Buscar...');
 
 
     clear(table: Table) {
